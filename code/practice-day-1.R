5ec255c0aba6ba0d8577bcf9e8ebc4c81596be1c
Lukáš Hejtmánek <hejtmy@gmail.com>
2023-07-26 09:24:21 +0200

    Adds new data and tasks for the morning practice

---
+++ code/practice-day-1.R
@@ -0,0 +1,26 @@
+# load in Life expectancy data
+
+# Check unique countries in the data and fix the problem
+
+# output all the column names
+
+# look at the summary of the data
+
+# Check what is the average Life.expectancy per country
+
+# Order the result from the lowest to the highest
+
+# Filter all data for only years 2000 and 2015
+
+# See which countries appear twice in this filtered data (use function count())
+
+# filter only for those countries where there are two recordings
+# This might have to be several lines of code and you might need to create extra
+# variables
+
+# Calculate difference for these countries in their life expectancy 
+#   between the min and the max
+
+# Select Columns from Country to Alcohol and then from GDP to Schooling
+
+# Select all columns except the Disease ones 
